8e446c91126d810e290f8e176feddb60f2c1aba2
cliffz <1063771265@qq.com>
2020-11-30 09:07:55 +0800

    3

--- nav/cfeeds/com/js/cfj.js
+++ nav/cfeeds/com/js/cfj.js
@@ -1,8 +1,14 @@
 
-function gotoScriptURL(e,dl=true){
+function gotoSPathURL(e,dl){
     var path = e.getAttribute('spath');
     var a=document.createElement('A');
     a.href = path;
     var dscriptUrl = dl?'feed://dscript/?url='+a.href : 'feed://olscript/?url='+a.href ;
     location.href=dscriptUrl;
+}
+function gotoDScriptURL(e){
+    gotoSPathURL(e,true);
+}
+function gotoOLScriptURL(e){
+    gotoSPathURL(e,false);
 }
